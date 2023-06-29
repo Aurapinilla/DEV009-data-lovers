1826195309822e8050cb57b1a22892a5c9b48e62
Aurapinilla <aura.pinilla12@gmail.com>
2023-06-29 17:53:45 -0500

    test completos

--- src/data.js
+++ src/data.js
@@ -12,133 +12,184 @@ export function filterTeam(datos, pais) {
 
 //FILTRO DEPORTE
 export function filterSport(datos, deporte) {
-  const g = datos.filter((obj) => {
-    return obj.sport === deporte;
-  })
-  return g;
+  if (Array.isArray(datos)) {
+    const g = datos.filter((obj) => {
+      return obj.sport === deporte;
+    })
+    return g;
+  } else {
+    return "ERROR DATA INVALIDA"
+  }
 }
 
 //FILTRO GENERO
 export function filterGen(datos, genero) {
-  const m = datos.filter((obj) => {
-    return obj.gender === genero;
-  })
-  return m
+  if (Array.isArray(datos)) {
+    const m = datos.filter((obj) => {
+      return obj.gender === genero;
+    })
+    return m
+  }
+  else {
+    return "ERROR DATA INVALIDA"
+  }
 }
 
 //FILTRO MEDALLA
 export function filterMedal(datos, medalla) {
-  const g = datos.filter((obj) => {
-    return obj.medal === medalla;
-  })
-  return g
+  if (Array.isArray(datos)) {
+    const g = datos.filter((obj) => {
+      return obj.medal === medalla;
+    })
+    return g
+  }
+  else {
+    return "ERROR DATA INVALIDA"
+  }
 }
 
 //ORDEN
 export const sortData = {
   //ordena los atletas por edad: Mayor a Menor
   sortbyAge1: (data) => {
-    const newdata = data.slice().sort((a, b) => {
-      if (b.age > a.age) {
-        return 1;
-      }
-      if (b.age < a.age) {
-        return -1;
-      }
-      return 0;
-    })
-    return newdata;
+    if (Array.isArray(data)) {
+      const newdata = data.slice().sort((a, b) => {
+        if (b.age > a.age) {
+          return 1;
+        }
+        if (b.age < a.age) {
+          return -1;
+        }
+        return 0;
+      })
+      return newdata;
+    }
+    else {
+      return "ERROR DATA INVALIDA"
+    }
   },
   //ordena los atletas por edad: Menor a Mayor
   sortbyAge2: (data) => {
-    const newdata = data.slice().sort((a, b) => {
-      if (a.age > b.age) {
-        return 1;
-      }
-      if (a.age < b.age) {
-        return -1;
-      }
-      return 0;
-    })
-    return newdata;
+    if (Array.isArray(data)) {
+      const newdata = data.slice().sort((a, b) => {
+        if (a.age > b.age) {
+          return 1;
+        }
+        if (a.age < b.age) {
+          return -1;
+        }
+        return 0;
+      })
+
+      return newdata;
+    }
+    else {
+      return "ERROR DATA INVALIDA"
+    }
   },
   //ordena los atletas por nombre A - Z
   sortbyNameAtoZ: (data) => {
-    const newdata = data.slice().sort((a, b) => {
-      const nameA = a.name.toLowerCase();
-      const nameB = b.name.toLowerCase();
+    if (Array.isArray(data)) {
+      const newdata = data.slice().sort((a, b) => {
+        const nameA = a.name.toLowerCase();
+        const nameB = b.name.toLowerCase();
 
-      if (nameA > nameB) {
-        return 1;
-      }
-      if (nameA < nameB) {
-        return -1;
-      }
-      return 0;
-    })
-    return newdata
+        if (nameA > nameB) {
+          return 1;
+        }
+        if (nameA < nameB) {
+          return -1;
+        }
+        return 0;
+      })
+      return newdata
+    }
+    else {
+      return "ERROR DATA INVALIDA"
+    }
   },
   //ordena los atletas por nombre Z - A
   sortbyNameZtoA: (data) => {
-    const newdata = data.slice().sort((a, b) => {
-      const nameA = a.name.toLowerCase();
-      const nameB = b.name.toLowerCase();
+    if (Array.isArray(data)) {
+      const newdata = data.slice().sort((a, b) => {
+        const nameA = a.name.toLowerCase();
+        const nameB = b.name.toLowerCase();
 
-      if (nameB > nameA) {
-        return 1;
-      }
-      if (nameB < nameA) {
-        return -1;
-      }
-      return 0;
-    })
-    return newdata
+        if (nameB > nameA) {
+          return 1;
+        }
+        if (nameB < nameA) {
+          return -1;
+        }
+        return 0;
+      })
+      return newdata
+    }
+    else {
+      return "ERROR DATA INVALIDA"
+    }
   },
   //ordena los datos por país A - Z
   sortbyCountryAtoZ: (data) => {
-    const newdata = data.slice().sort((a, b) => {
-      const teamA = a.team.toLowerCase();
-      const teamB = b.team.toLowerCase();
+    if (Array.isArray(data)) {
+      const newdata = data.slice().sort((a, b) => {
+        const teamA = a.team.toLowerCase();
+        const teamB = b.team.toLowerCase();
 
-      if (teamA > teamB) {
-        return 1;
-      }
-      if (teamA < teamB) {
-        return -1;
-      }
-      return 0;
-    })
-    return newdata
+        if (teamA > teamB) {
+          return 1;
+        }
+        if (teamA < teamB) {
+          return -1;
+        }
+        return 0;
+      })
+      return newdata
+    }
+    else {
+      return "ERROR DATA INVALIDA"
+    }
   },
   //ordena los datos por país Z - A
   sortbyCountryZtoA: (data) => {
-    const newdata = data.slice().sort((a, b) => {
-      const teamA = a.team.toLowerCase();
-      const teamB = b.team.toLowerCase();
+    if (Array.isArray(data)) {
+      const newdata = data.slice().sort((a, b) => {
+        const teamA = a.team.toLowerCase();
+        const teamB = b.team.toLowerCase();
 
-      if (teamB > teamA) {
-        return 1;
-      }
-      if (teamB < teamA) {
-        return -1;
-      }
-      return 0;
-    })
-    return newdata
-  },
+        if (teamB > teamA) {
+          return 1;
+        }
+        if (teamB < teamA) {
+          return -1;
+        }
+        return 0;
+      })
+      return newdata
+    }
+    else {
+      return "ERROR DATA INVALIDA"
+    }
+  }
 };
 
 // estadistica
 export function estadistica(medallas) {
-  const suma = medallas.gold + medallas.silver + medallas.bronze
-  const gold = (((medallas.gold * 100) / suma)).toFixed(2)
-  const silver = (((medallas.silver * 100) / suma)).toFixed(2)
-  const bronze = (((medallas.bronze * 100) / suma)).toFixed(2)
+  if (typeof medallas === 'object') {
 
-  const porcentaje = {
-    goldPorcent: parseFloat(gold),
-    silverPorcent: parseFloat(silver),
-    bronzePorcent: parseFloat(bronze),
+    const suma = medallas.gold + medallas.silver + medallas.bronze
+    const gold = (((medallas.gold * 100) / suma)).toFixed(2)
+    const silver = (((medallas.silver * 100) / suma)).toFixed(2)
+    const bronze = (((medallas.bronze * 100) / suma)).toFixed(2)
+
+    const porcentaje = {
+      goldPorcent: parseFloat(gold),
+      silverPorcent: parseFloat(silver),
+      bronzePorcent: parseFloat(bronze),
+    }
+    return porcentaje
   }
-  return porcentaje
+  else {
+    return "ERROR DATA INVALIDA"
+  }
 }
--- test/data.spec.js
+++ test/data.spec.js
@@ -75,17 +75,134 @@ describe('ordena por', () => {
       {"name": "VIRGINIA ANTHONY","sport": "Swimming","team": "Colombia","age": 40, "gender": "F", "medal": "Bronze"},
     ]);
   });
+
+  it ("Deberia devolver ERROR DATA INVALIDA cuando se le pasa un dato que no sea un arreglo", () => {
+    const input = {"name": "Patimat Abakarova","gender": "F","sport": "Volleyball","team": "United States"}
+    const output = "ERROR DATA INVALIDA"
+    expect (sortData.sortbyAge1(input)).toEqual(output);
+  })
+
+  it ("Deberia devolver ERROR DATA INVALIDA cuando se le pasa un dato que no sea un arreglo", () => {
+    const input = {"name": "Patimat Abakarova","age": 34}
+    const output = "ERROR DATA INVALIDA"
+    expect (sortData.sortbyAge2(input)).toEqual(output);
+  })
+
+  it ("Deberia devolver ERROR DATA INVALIDA cuando se le pasa un dato que no sea un arreglo", () => {
+    const input = {"name": "Patimat Abakarova","gender": "F","sport": "Volleyball","team": "United States"}
+    const output = "ERROR DATA INVALIDA"
+    expect (sortData.sortbyNameAtoZ(input)).toEqual(output);
+  })
+
+  it ("Deberia devolver ERROR DATA INVALIDA cuando se le pasa un dato que no sea un arreglo", () => {
+    const input = {"name": "Patimat Abakarova","gender": "F","sport": "Volleyball","team": "United States"}
+    const output = "ERROR DATA INVALIDA"
+    expect (sortData.sortbyNameZtoA(input)).toEqual(output);
+  })
+
+  it ("Deberia devolver ERROR DATA INVALIDA cuando se le pasa un dato que no sea un arreglo", () => {
+    const input = "United States"
+    const output = "ERROR DATA INVALIDA"
+    expect (sortData.sortbyCountryAtoZ(input)).toEqual(output);
+  })
+
+  it ("Deberia devolver ERROR DATA INVALIDA cuando se le pasa un dato que no sea un arreglo", () => {
+    const input = "Colombia"
+    const output = "ERROR DATA INVALIDA"
+    expect (sortData.sortbyCountryZtoA(input)).toEqual(output);
+  })
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+describe('filterTeam', () => {
+  it('Pide mostrar el país Italia', () => {
+    expect(filterTeam(data, 'Italy')).toEqual([{"name": "Giovanni Abagnale","gender": "M","sport": "Gymnastics","team": "Italy","medal": "Bronze","age":42},
+      {"name": "Josue Glover","sport": "Basketball","team": "Italy","age": 19, "gender": "M", "medal": "Silver"}]);
+  });
+  it('Pide mostrar el país United States', () => {
+    expect(filterTeam(data, "United States")).toEqual([{"name": "Patimat Abakarova","gender": "F","sport": "Volleyball","team": "United States","medal": "Bronze","age":15},]);
+  });
+  it('Pide mostrar el país Colombia', () => {
+    expect(filterTeam(data, "Colombia")).toEqual([{"name": "VIRGINIA ANTHONY","sport": "Swimming","team": "Colombia","age": 40, "gender": "F", "medal": "Bronze"},]);
+  });
+  it('Pide mostrar el país Russia', () => {
+    expect(filterTeam(data, "Russia")).toEqual([{"name": "Ahmed Merritt","sport": "Football","team": "Russia","age": 30, "gender": "M", "medal": "Gold"},]);
+  });
+  it('Pide mostrar el país France', () => {
+    expect(filterTeam(data, "France")).toEqual([{"name": "emmalyn dunn","sport": "Swimming","team": "France","age": 22, "gender": "F", "medal": "Gold"},]);
+  });
+  it("Deberia devolver ERROR DATA INVALIDA cuando se le pasa un dato que no sea un arreglo",()=>{
+    const input = 10
+    const output = "ERROR DATA INVALIDA"
+    expect(filterTeam(input,"Italy")).toBe(output)
+  })
+});
+
+describe('filterSport',()=> {
+  it("Pide filtrar por el deporte Volleyball",()=>{
+    expect(filterSport(data,"Volleyball")).toEqual([{"name": "Patimat Abakarova","gender": "F","sport": "Volleyball","team": "United States","medal": "Bronze","age":15},])
+  });
+  it("Pide filtrar por el deporte Gymnastics",()=>{
+    expect(filterSport(data,"Gymnastics")).toEqual([{"name": "Giovanni Abagnale","gender": "M","sport": "Gymnastics","team": "Italy","medal": "Bronze","age":42},])
+  });
+  it("Pide filtrar por el deporte Football",()=>{
+    expect(filterSport(data,"Football")).toEqual([{"name": "Ahmed Merritt","sport": "Football","team": "Russia","age": 30, "gender": "M", "medal": "Gold"},])
+  });
+  it("Pide filtrar por el deporte Basketball",()=>{
+    expect(filterSport(data, "Basketball")).toEqual([{"name": "Josue Glover","sport": "Basketball","team": "Italy","age": 19, "gender": "M", "medal": "Silver"},])
+  });
+  it("Pide filtrar por el deporte Swimming",()=>{
+    expect(filterSport(data, "Swimming")).toEqual([{"name": "emmalyn dunn","sport": "Swimming","team": "France","age": 22, "gender": "F", "medal": "Gold"},
+      {"name": "VIRGINIA ANTHONY","sport": "Swimming","team": "Colombia","age": 40, "gender": "F", "medal": "Bronze"},])
+  });
+  it('Deberia devolder ERROR DATA INVALIDA cuando se le pase un dato que no sea un arreglo', ()=> {
+    const input = 20
+    const output = "ERROR DATA INVALIDA"
+    expect(filterSport(input,"Football")).toBe(output)
+  })
+});
+
+describe('filterGen',()=> {
+  it("Pide filtrar por Genero Femenino",()=>{
+    expect(filterGen(data,"F")).toEqual([
+      {"name": "Patimat Abakarova","gender": "F","sport": "Volleyball","team": "United States","medal": "Bronze","age":15},
+      {"name": "emmalyn dunn","sport": "Swimming","team": "France","age": 22, "gender": "F", "medal": "Gold"},
+      {"name": "VIRGINIA ANTHONY","sport": "Swimming","team": "Colombia","age": 40, "gender": "F", "medal": "Bronze"}
+    ]);
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it("Pide filtrar por Genero Masculino",()=>{
+    expect(filterGen(data,"M")).toEqual([
+      {"name": "Giovanni Abagnale","gender": "M","sport": "Gymnastics","team": "Italy","medal": "Bronze","age":42},
+      {"name": "Josue Glover","sport": "Basketball","team": "Italy","age": 19, "gender": "M", "medal": "Silver"},
+      {"name": "Ahmed Merritt","sport": "Football","team": "Russia","age": 30, "gender": "M", "medal": "Gold"},
+    ]);
   });
+  it('Deberia devolder ERROR DATA INVALIDA cuando se le pase un dato que no sea un arreglo', ()=> {
+    const input = 'A'
+    const output = "ERROR DATA INVALIDA"
+    expect(filterGen(input,"M")).toBe(output)
+  })
+});
+
+describe('filterMedal',()=> {
+  it("Pide filtrar por Medalla de Oro",()=>{
+    expect(filterMedal(data,"Gold")).toEqual([{"name": "Ahmed Merritt","sport": "Football","team": "Russia","age": 30, "gender": "M", "medal": "Gold"},
+      {"name": "emmalyn dunn","sport": "Swimming","team": "France","age": 22, "gender": "F", "medal": "Gold"},])
+  });
+  it("Pide filtrar por Medalla de Plata",()=>{
+    expect(filterMedal(data,"Silver")).toEqual([{"name": "Josue Glover","sport": "Basketball","team": "Italy","age": 19, "gender": "M", "medal": "Silver"},])
+  });
+  it("Pide filtrar por Medalla de bronze",()=>{
+    expect(filterMedal(data,"Bronze")).toEqual([{"name": "Giovanni Abagnale","gender": "M","sport": "Gymnastics","team": "Italy","medal": "Bronze","age":42},
+      {"name": "Patimat Abakarova","gender": "F","sport": "Volleyball","team": "United States","medal": "Bronze","age":15},
+      {"name": "VIRGINIA ANTHONY","sport": "Swimming","team": "Colombia","age": 40, "gender": "F", "medal": "Bronze"},])
+  });
+  it('Deberia devolder ERROR DATA INVALIDA cuando se le pase un dato que no sea un arreglo', ()=> {
+    const input = 123
+    const output = "ERROR DATA INVALIDA"
+    expect(filterMedal(input,"Bronze")).toBe(output)
+  })
 });
 
 describe('estadistica', () => {
@@ -93,4 +210,10 @@ describe('estadistica', () => {
     expect(estadistica(medals).bronzePorcent).toEqual(53.75);
     expect(estadistica(medals).goldPorcent).toEqual(25.00);
     expect(estadistica(medals).silverPorcent).toEqual(21.25)})
+
+  it ('Deberia devolder ERROR DATA INVALIDA cuando se le pase un dato que no sea un arreglo', () => {
+    const input = 53
+    const output = "ERROR DATA INVALIDA"
+    expect(estadistica(input)).toEqual(output)
+  })
 });
